6e727508030405791cdf7800c325bc4572de2ba8
IllegalDoc <anis921787@gmail.com>
2025-09-04 15:14:23 +0100

    finished header almost done with searchbar

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,19 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { G, Path, Svg } from "react-native-svg";
 
 export default function Index() {
+  const [searchBarValue, SetsearchBarValue] = useState("");
+
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <View style={{ height: 72, flexDirection: "row" }}>
-          <Text style={{}}> Gam Lab Pharma</Text>
+        <View style={styles.headerContainer}>
+          <View></View>
+          <Text style={styles.headerName}>Gam Lab Pharma</Text>
           <Svg
+            style={styles.headerCart}
             fill="#000000"
             stroke="#000000"
             strokeWidth={0.009}
@@ -33,7 +38,55 @@ export default function Index() {
             </G>
           </Svg>
         </View>
+        <View style={styles.SearchbarContainer}>
+          <View>
+            <View style={styles.Searchbar}>
+              <Svg
+                style={styles.SearchbarSVG}
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+              >
+                <Path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M19.5306 18.4694L14.8366 13.7762C17.6629 10.383 17.3204 5.36693 14.0591 2.38935C10.7978 -0.588237 5.77134 -0.474001 2.64867 2.64867C-0.474001 5.77134 -0.588237 10.7978 2.38935 14.0591C5.36693 17.3204 10.383 17.6629 13.7762 14.8366L18.4694 19.5306C18.7624 19.8237 19.2376 19.8237 19.5306 19.5306C19.8237 19.2376 19.8237 18.7624 19.5306 18.4694ZM1.75 8.5C1.75 4.77208 4.77208 1.75 8.5 1.75C12.2279 1.75 15.25 4.77208 15.25 8.5C15.25 12.2279 12.2279 15.25 8.5 15.25C4.77379 15.2459 1.75413 12.2262 1.75 8.5Z"
+                  fill="#4F964F"
+                />
+              </Svg>
+              <TextInput
+                style={styles.Searchbarinput}
+                placeholder="Rechercher  produits, marques, catégories"
+                placeholderTextColor={"#4F964F"}
+                value={searchBarValue}
+                onChangeText={SetsearchBarValue}
+              ></TextInput>
+            </View>
+          </View>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: { height: 72, flexDirection: "row", padding: 10 },
+  headerName: { fontSize: 18, textAlign: "center", flex: 1, marginLeft: 10 },
+  headerCart: { marginRight: 5 },
+  SearchbarContainer: { height: 90, padding: 20, justifyContent: "center" },
+  Searchbarinput: { overflow: "hidden" },
+  Searchbar: {
+    overflow: "hidden",
+    alignItems: "center",
+    gap: 10,
+    height: 85,
+    backgroundColor: "#E8F2E8",
+    borderRadius: 25,
+    flexDirection: "row",
+  },
+  SearchbarSVG: {
+    marginLeft: 16,
+  },
+});
